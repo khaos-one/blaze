ecc3a6acb193a752c2a8cf035cf32e6628661cb5
Егор Зеленский <i@khaos.su>
2015-09-03 19:38:52 +0300

    Add very basic compiler functionality

---
+++ Src/AppConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blaze
+{
+    [Serializable]
+    internal sealed class AppConfiguration
+    {
+        public string CompilerVersion { get; set; }
+        public string[] References { get; set; }
+    }
+}
--- Src/Program.cs
+++ Src/Program.cs
@@ -1,13 +1,44 @@
-﻿using System;
+﻿using Microsoft.CSharp;
+using Newtonsoft.Json;
+using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Threading;
 
 namespace Blaze
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
+            var mainThread = new Thread(MainThread);
+            mainThread.Start(args);
+        }
+
+        internal static void MainThread(object state)
+        {
+            var args = (string[])state;
+
+            if (args.Length < 2)
+                throw new Exception("Dunno what to run.");
+
+            var appDirectory = Path.GetFullPath(args[1]);
+            var appFile = Path.Combine(appDirectory, "App.cs");
+            var appConfigFile = Path.Combine(appDirectory, "BlazeApp.json.config");
+
+            if (!File.Exists(appConfigFile))
+                throw new Exception("App configuration file was not found.");
+            if (!File.Exists(appFile))
+                throw new Exception("No app entry found.");
+
+            var appConfigFileContent = File.ReadAllText(appConfigFile);
+            var appConfig = JsonConvert.DeserializeObject<AppConfiguration>(appConfigFileContent);
+
+            var tempAssemblyPath = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
+            var codeProvider = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", appConfig.CompilerVersion } });
+            var compilerParameters = new CompilerParameters(appConfig.References, tempAssemblyPath, false);
+            var compilerResult = codeProvider.CompileAssemblyFromFile(compilerParameters, new[] { appFile });
         }
     }
 }
