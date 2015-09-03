cea8f8478f58a96c8508b56323eb422d73eef614
Егор Зеленский <i@khaos.su>
2015-09-03 20:29:08 +0300

    Add

--- Src/Program.cs
+++ Src/Program.cs
@@ -39,8 +39,16 @@ namespace Blaze
 
             var tempAssemblyPath = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
             var codeProvider = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", appConfig.CompilerVersion } });
-            var compilerParameters = new CompilerParameters(appConfig.References, tempAssemblyPath, false);
+            var compilerParameters = new CompilerParameters(appConfig.References, tempAssemblyPath, false)
+            {
+                GenerateInMemory = true
+            };
             var compilerResult = codeProvider.CompileAssemblyFromFile(compilerParameters, new[] { appFile });
+
+            if (compilerResult.Errors.Count > 0)
+                throw new Exception("There were errors during compilation.");
+
+            // TODO: Invoke compiled entry method -- http://stackoverflow.com/questions/14479074/c-sharp-reflection-load-assembly-and-invoke-a-method-if-it-exists
         }
     }
 }
